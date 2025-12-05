9bddc7a5df52a4419f59a81b3ea1493836c9be7c
Lucas Rigo <lucas.rigo@numenit.com>
2025-12-05 11:16:58 -0300

    + day 5 part 2

--- day5/cafeteria.js
+++ day5/cafeteria.js
@@ -60,10 +60,95 @@ var part1 = function() {
 var part2 = function () {
 
   for (let i = 0; i < input.length; i++) {
-    const numberStrings = input[i].split(/\s+/)
-    const numbers = numberStrings.map((val => {return Number(val)}))
+    const inputParts = input[i].split('\n\n') // separate
+    const ingredientIdRanges = inputParts[0].split('\n').map(line => {
+      const parts = line.split('-')
+      return {
+        from: Number(parts[0]),
+        to: Number(parts[1])
+      }
+    })
 
-    const result = 0
+    ingredientIdRanges.sort((a, b) => {
+      if (a.from === b.from) {
+        return a.to - b.to
+      }
+      return a.from - b.from
+    })
+
+    let freshRanges = ingredientIdRanges.slice(0)
+    let mergeCount = -1
+    while (mergeCount !== 0) {
+      mergeCount = 0
+      let newRanges = []
+      freshRanges.forEach(fr1 => {
+        // insert in correct place
+        if (newRanges.length === 0) {
+          newRanges.push(fr1)
+          return true // continue
+        }
+        let insertIdx = newRanges.findIndex(fr => fr.from > fr1.from)
+        if (insertIdx < 0) {
+          insertIdx = newRanges.length
+        }
+        // check overlap with previous
+        if (insertIdx > 0) {
+          const prevRange = newRanges[insertIdx - 1]
+          // 12-14 and 10-13
+          if (prevRange.to >= fr1.from) { // merge
+            mergeCount++
+            const newRange = {
+              from: Math.min(fr1.from, prevRange.from),
+              to: Math.max(fr1.to, prevRange.to)
+            }
+            newRanges.splice(insertIdx - 1, 1, newRange)
+            return true // continue
+          }
+        }
+        // check overlap with next
+        if (insertIdx < newRanges.length) {
+          const nextRange = newRanges[insertIdx]
+          // 12-16 and 14-16
+          if (fr1.to === nextRange.to) { // merge
+            mergeCount++
+            const newRange = {
+              from: Math.min(fr1.from, nextRange.from), // Min(12,14)
+              to: fr1.to // 16
+            }
+            newRanges.splice(insertIdx, 1, newRange)
+            return true // continue
+          }
+          // 10-14 and 10-16
+          if (fr1.from === nextRange.from) { // merge
+            mergeCount++
+            const newRange = {
+              from: fr1.from, // 10
+              to: Math.max(fr1.to, nextRange.to) // Max(14,16)
+            }
+            newRanges.splice(insertIdx, 1, newRange)
+            return true // continue
+          }
+          // 12-16 and 14-18
+          if (nextRange.from <= fr1.to) { // merge
+            mergeCount++
+            const newRange = {
+              from: Math.min(fr1.from, nextRange.from),
+              to: Math.max(fr1.to, nextRange.to)
+            }
+            newRanges.splice(insertIdx, 1, newRange)
+            return true // continue
+          }
+        }
+        // else just insert
+        newRanges.splice(insertIdx, 0, fr1)
+      })
+      freshRanges = newRanges.slice()
+      // console.log(freshRanges)
+    }
+
+    const result = freshRanges.reduce((acc, range) => {
+      return acc + (range.to - range.from + 1)
+    },0)
     // console.log(result)
     $('#part2').append(input[i])
       .append('<br>&emsp;')
