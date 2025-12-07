b1ad14228c4054cb51a15b9d601ffbdc7d6a9dc8
Lucas Rigo <lucas.rigo@numenit.com>
2025-12-07 08:24:50 -0300

    + day 6 part 2

--- day6/trashCompactor.js
+++ day6/trashCompactor.js
@@ -1,6 +1,6 @@
 var input = [
-`123 328  51 64
- 45 64  387 23
+`123 328  51 64 `+`
+ 45 64  387 23 `+`
   6 98  215 314
 *   +   *   +  `
  ,puzzleInput
@@ -49,10 +49,57 @@ var part1 = function() {
 var part2 = function () {
 
   for (let i = 0; i < input.length; i++) {
-    const numberStrings = input[i].split(/\s+/)
-    const numbers = numberStrings.map((val => {return Number(val)}))
+    const rowStrings = input[i].split('\n')
+    // find out max col lenght based on last line
+    const homework = []
+    let lenCounter = 1
+    let calc = {op: rowStrings[rowStrings.length-1][0], vals: []}
+    for (let c = 1; c < rowStrings[rowStrings.length-1].length; c++) {
+      const char = rowStrings[rowStrings.length-1][c]
+      if (char !== ' ') {
+        calc.len = lenCounter-1 // -1 space separator
+        homework.push(calc)
+        // reset calc
+        calc = {op: char, vals: []}
+        lenCounter = 1
+      } else {
+        lenCounter++
+      }
+    }
+    calc.len = lenCounter
+    homework.push(calc)
 
-    const result = 0
+    let hi = 0
+    for (let r = 0; r < rowStrings.length-1; r++) {
+      const line = rowStrings[r].split('');
+      hi = 0
+      for (let l = 0; l < line.length; l++) {
+        homework[hi].vals.push(line.slice(l, l+homework[hi].len))
+        l += homework[hi].len
+        hi++
+      }
+    }
+    // console.log(homework)
+    homework.forEach((calc) => {
+      calc.realVals = []
+      for (let n = calc.len-1; n >=0; n--) { // parse numbers from right to left
+        let numStr = ''
+        for (let v = 0; v < calc.vals.length; v++) {
+          numStr += calc.vals[v][n]
+        }
+        calc.realVals.push(Number(numStr))
+      }
+      calc.result = calc.realVals.reduce((a, b) => {
+        if (calc.op === '+') {
+          return a + b
+        } else if (calc.op === '*') {
+          return a * b
+        }
+      })
+    })
+    // console.log(homework)
+
+    const result = homework.reduce((acc, calc) => acc + calc.result, 0)
     // console.log(result)
     $('#part2').append(input[i])
       .append('<br>&emsp;')
