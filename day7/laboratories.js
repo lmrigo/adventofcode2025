5b86595a9d4b2e46c34c1267af40b3dc1bb2bf12
Lucas Rigo <lucas.rigo@numenit.com>
2025-12-08 11:07:51 -0300

    + day 7 part 2

--- day7/laboratories.js
+++ day7/laboratories.js
@@ -72,10 +72,67 @@ function printGrid(grid) {
 var part2 = function () {
 
   for (let i = 0; i < input.length; i++) {
-    const numberStrings = input[i].split(/\s+/)
-    const numbers = numberStrings.map((val => {return Number(val)}))
+    const rows = input[i].split(/\n/)
+    let grid = rows.map((val => val.split('')))
+    const splitsPerLine = []
+    rows.forEach((val => {
+      const splits = val.match(/\^/g)?.length
+      if (splits) {
+        splitsPerLine.push(splits)
+      }
+    }))
+    // console.log(splitsPerLine)
 
-    const result = 0
+    const start = {y: 0, x: grid[0].indexOf('S')}
+    grid[start.y][start.x] = '.'
+    // console.log(start,grid)
+
+    const fall = function(y, x) {
+      let splitCount = 0
+      if (y >= grid.length) {
+        return splitCount
+      } else {
+        if (grid[y][x] === '^') {
+          const leftSplits = fall(y, x-1)
+          grid[y][x-1] = '|'
+          const rightSplits = fall(y, x+1)
+          grid[y][x+1] = '|'
+          splitCount += 1 + leftSplits + rightSplits
+        } else if (grid[y][x] === '.') {
+          grid[y][x] = '|'
+          splitCount += fall(y+1, x)
+        } // else if (grid[y][x] === '^') { it's already traversed
+        return splitCount
+      }
+    }
+    fall(start.y, start.x)
+    // printGrid(grid)
+
+    // setup start
+    grid[grid.length-1].forEach((val,x) => grid[grid.length-1][x] = val === '|' ? 1 : '.')
+    // calculate upwards
+    for (let y = grid.length-2; y >= 0; y--) {
+      for (let x = 0; x < grid[y].length; x++) {
+        if (grid[y][x] === '|') {
+          if (grid[y+1][x] === '^') {
+            grid[y][x] = grid[y+1][x-1] + grid[y+1][x+1]
+          } else {
+            grid[y][x] = grid[y+1][x]
+          }
+          // if (grid[y][x+1] === '^') {
+          //   grid[y][x] += 1
+          // }
+          // if (grid[y][x-1] === '^') {
+          //   grid[y][x] += 1
+          // }
+        }
+      }
+    }
+    // printGrid(grid)
+
+    const timelines = grid[0][start.x]
+
+    const result = timelines
     // console.log(result)
     $('#part2').append(input[i])
       .append('<br>&emsp;')
